8cd67438a57918232c479d5b5254b088fca2c3f9
bonzoman <bonzoman@gmail.com>
2023-07-19 07:10:51 +0900

    sssssssssss

--- t.py
+++ t.py
@@ -1,8 +1,8 @@
-startNo, endNo = 0, 102
-for i in range(startNo, endNo):  # 1부터 100까지의 숫자를 반복
-    if i == startNo or i % 10 == 0:
-        print("현재 숫자는", i, "입니다.")
+car = {"name1": "BMW", "name2": {"price1": "7000", "price2": "7000"}}
 
-# 마지막에 한 번 더 실행하는 코드
-if not i % 10 == 0:
-    print("코드 실행이 종료되었습니다.", i)
+value = car.get("company")
+
+if 'price1' in car:
+    print("Key exist! The value is " + car["price1"])
+else:
+    print('Key not exist!')
--- test2.py
+++ test2.py
@@ -1,8 +1,11 @@
+import sys
+
 import requests
 from bs4 import BeautifulSoup
-# from openpyxl import Workbook
+from openpyxl import Workbook
 import pandas as pd
 from datetime import datetime
+from openpyxl.cell.cell import ILLEGAL_CHARACTERS_RE
 
 
 def initialize_variables():
@@ -17,6 +20,7 @@ def save_data_to_excel(index):
                    '마감기간': due_time,
                    'position': position,
                    'skill_tags': skill_tags,
+                   '국가': country,
                    '지역': location,
                    'hidden': hidden,
                    'status': status,
@@ -30,16 +34,16 @@ def save_data_to_excel(index):
                    }
 
     # 두 번째 시트 데이터
-    data_sheet2 = {'skill_tags': list(set(skill_tags_all_list))}  # 중복제거
+    # data_sheet2 = {'skill_tags': list(set(skill_tags_all_list))}  # 중복제거
 
     # DataFrame 객체 생성
     df1 = pd.DataFrame(data_sheet1)
-    df2 = pd.DataFrame(data_sheet2)
+    # df2 = pd.DataFrame(data_sheet2)
 
     # Excel 파일로 저장
     with pd.ExcelWriter('WantedInfo' + str(idx) + '.xlsx') as writer:
         df1.to_excel(writer, sheet_name='시트1', index=False)
-        df2.to_excel(writer, sheet_name='시트2', index=False)
+        # df2.to_excel(writer, sheet_name='시트2', index=False)
 
 
 # START #########################################################################################################
@@ -50,17 +54,19 @@ API_URL = 'https://www.wanted.co.kr/api/v4/jobs/'
 SITE_URL = 'https://www.wanted.co.kr/wd/'
 
 # startNo, endNo, interval = 170001, 170012, 1000
-startNo, endNo, interval = 102, 100001, 1000
+startNo, endNo, interval = 8901, 20000, 10000
 
-page, company, industry_name, due_time, position, skill_tags, location, hidden, status, address, \
+page, company, industry_name, due_time, position, skill_tags, country, location, hidden, status, address, \
     detail_requirements, detail_main_tasks, detail_intro, detail_benefits, detail_preferred_points, \
-    category_tags, skill_tags_all_list = initialize_variables()
-
+    category_tags = initialize_variables()
+idx = 0
 for idx in range(startNo, endNo):
+    response = requests.get(API_URL + str(idx))
+    if not response.ok:
+        continue
 
-    response = requests.get(API_URL+str(idx))
     responseJson = response.json()
-    print(SITE_URL+str(idx), responseJson)
+    print(SITE_URL + str(idx), responseJson)
 
     if responseJson.__contains__('job'):
 
@@ -68,49 +74,102 @@ for idx in range(startNo, endNo):
         jobKeys = jobObj.keys()
         # print(jobObj)
 
-        page.append(SITE_URL+str(idx))
+        page.append(SITE_URL + str(idx))
 
-        for n in jobKeys:
-            if n == 'company':
-                company.append(jobObj[n]['name'])
-                industry_name.append(jobObj[n]['industry_name'])
-            elif n == 'due_time':
-                due_time.append(jobObj.get(n))
-            elif n == 'position':
-                position.append(jobObj.get(n))
-            elif n == 'skill_tags':
+        # key 없는 경우 미리 초기화 처리... array length 맞춰야 함
+        company.append('') if 'company' not in jobObj else None
+        industry_name.append('') if 'company' not in jobObj else None
+        due_time.append('') if 'due_time' not in jobObj else None
+        position.append('') if 'position' not in jobObj else None
+        skill_tags.append('') if 'skill_tags' not in jobObj else None
+        category_tags.append('') if 'category_tags' not in jobObj else None
+        country.append('') if 'address' not in jobObj else None
+        location.append('') if 'address' not in jobObj else None
+        address.append('') if 'address' not in jobObj else None
+        hidden.append('') if 'hidden' not in jobObj else None
+        status.append('') if 'status' not in jobObj else None
+        detail_requirements.append('') if 'detail' not in jobObj else None
+        detail_main_tasks.append('') if 'detail' not in jobObj else None
+        detail_intro.append('') if 'detail' not in jobObj else None
+        detail_benefits.append('') if 'detail' not in jobObj else None
+        detail_preferred_points.append('') if 'detail' not in jobObj else None
+
+        for key in jobKeys:
+            if key == 'company':
+                company.append(jobObj[key]['name'] if 'name' in jobObj[key] else '')
+                industry_name.append(jobObj[key]['industry_name'] if 'industry_name' in jobObj[key] else '')
+
+            if key == 'due_time':
+                due_time.append(jobObj.get(key) if not jobObj[key] is None else '상시')
+
+            if key == 'position':
+                position.append(jobObj.get(key))
+
+            if key == 'skill_tags':
                 aaa = []
-                for n2 in jobObj[n]:
-                    aaa.append(n2['title'])
-                    skill_tags_all_list.append(n2['title'])  # 전체 담기
+                for n2 in jobObj[key]:
+                    aaa.append(n2['title'] if 'title' in n2 else None)
+                    # skill_tags_all_list.append(n2['title'])  # 전체 담기
                 skill_tags.append(','.join(aaa))
-            elif n == 'category_tags':
+
+            if key == 'category_tags':
                 bbb = []
-                for n3 in jobObj[n]:
+                for n3 in jobObj[key]:
                     bbb.append(str(n3['parent_id']) + '.' + str(n3['id']))
                 category_tags.append(','.join(bbb))
-            elif n == 'address':
-                location.append(jobObj[n]['location'])
-                address.append(jobObj[n]['full_location'])
-            elif n == 'hidden':
-                hidden.append(jobObj.get(n))
-            elif n == 'status':
-                status.append(jobObj.get(n))
-            elif n == 'detail':
-                detail_requirements.append(jobObj[n]['requirements'])
-                detail_main_tasks.append(jobObj[n]['main_tasks'])
-                detail_intro.append(jobObj[n]['intro'])
-                detail_benefits.append(jobObj[n]['benefits'])
-                detail_preferred_points.append(jobObj[n]['preferred_points'])
+            if key == 'address':
+                country.append(jobObj[key]['country'] if 'country' in jobObj[key] else '')
+                location.append(jobObj[key]['location'] if 'location' in jobObj[key] else '')
+                address.append(jobObj[key]['full_location'] if 'full_location' in jobObj[key] else '')
+            if key == 'hidden':
+                hidden.append(jobObj.get(key))
+            if key == 'status':
+                status.append(jobObj.get(key))
+            if key == 'detail':
+                if jobObj[key] is None:
+                    detail_requirements.append('')
+                    detail_main_tasks.append('')
+                    detail_intro.append('')
+                    detail_benefits.append('')
+                    detail_preferred_points.append('')
+                else:
+                    # IllegalCharacterError 나서 ILLEGAL_CHARACTERS_RE 특수문자 제거
+                    detail_requirements.append(
+                        ILLEGAL_CHARACTERS_RE.sub(r'', jobObj[key]['requirements'])
+                        if 'requirements' in jobObj[key] else '')
+                    detail_main_tasks.append(
+                        ILLEGAL_CHARACTERS_RE.sub(r'', jobObj[key]['main_tasks'])
+                        if 'main_tasks' in jobObj[key] else '')
+                    detail_intro.append(
+                        ILLEGAL_CHARACTERS_RE.sub(r'', jobObj[key]['intro'])
+                        if 'intro' in jobObj[key] else '')
+                    detail_benefits.append(
+                        ILLEGAL_CHARACTERS_RE.sub(r'', jobObj[key]['benefits'])
+                        if 'benefits' in jobObj[key] else '')
+                    detail_preferred_points.append(
+                        ILLEGAL_CHARACTERS_RE.sub(r'', jobObj[key]['preferred_points'])
+                        if 'preferred_points' in jobObj[key] else '')
+        # for end
+
+        listLens = [len(page), len(company), len(industry_name), len(due_time), len(position),
+                    len(skill_tags), len(country), len(location), len(hidden), len(status), len(address),
+                    len(detail_requirements), len(detail_main_tasks), len(detail_intro), len(detail_benefits),
+                    len(detail_preferred_points), len(category_tags)  # , len(skill_tags_all_list)
+                    ]
+        if not len(set(listLens)) == 1:  # 길이가 다르면
+            print("오류")
+            sys.exit()
+
+    # if end
 
     if idx % interval == 0 and len(page) > 0:  # interval 배수인지 확인
         save_data_to_excel(idx)
-        page, company, industry_name, due_time, position, skill_tags, location, hidden, status, address, \
+        page, company, industry_name, due_time, position, skill_tags, country, location, hidden, status, address, \
             detail_requirements, detail_main_tasks, detail_intro, detail_benefits, detail_preferred_points, \
-            category_tags, skill_tags_all_list = initialize_variables()
+            category_tags = initialize_variables()
 
 # 마지막에 남은거 한 번 더 실행하는 코드
-if not idx % interval == 0 and len(page) > 0:   # interval 배수인지 확인
+if len(page) > 0:  # interval 배수인지 확인
     save_data_to_excel(idx)
 
 print("end : ", datetime.now())
